d480b452af9b724bdab17e1fcffc56a76665157a
kerolozz <keroloss244@gmail.com>
2023-08-23 12:24:36 -0700

    a

---
+++ getenv.c
@@ -0,0 +1,94 @@
+#include "shellkr.h"
+
+/**
+ * get_environ - returns the string array copy of our environ
+ * @inf: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+char **get_environkr(inf_t *inf)
+{
+	if (!inf->environkr || inf->env_changed)
+	{
+		inf->environkr = list_to_strings(inf->env);
+		inf->env_changed = 0;
+	}
+
+	return (inf->environkr);
+}
+
+/**
+ * _unsetenv - Remove an environment variable
+ * @inf: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _unsetenv(inf_t *inf, char *var)
+{
+	list_t *node = inf->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			inf->env_changed = delete_node_at_index(&(inf->env), i);
+			i = 0;
+			node = inf->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (inf->env_changed);
+}
+
+/**
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @inf: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ *  Return: Always 0
+ */
+int _setenv(inf_t *inf, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = inf->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			inf->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(inf->env), buf, 0);
+	free(buf);
+	inf->env_changed = 1;
+	return (0);
+}
+
